c41f0f4ed55c82169af046e88335c8d1adda0544
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-06-04 13:21:32 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,24 @@
+<p align="center">
+<b> ˹ᴛʜᴇ ʜᴀɴᴄᴏҡʏ ʙσᴛ˼ 🇷🇺</b>
+</p>
+
+<p align="center"><a href="https://t.me/TeleBotsUpdate"><img src="https://telegra.ph/file/3443b024b47952fd9aedf.jpg" width="300"></a></p>
+<p align="center">
+
+
+<h3 align="center">
+    ─ ᴅᴇᴩʟᴏʏ ᴏɴ ʜᴇʀᴏᴋᴜ ─
+</h3>
+
+<p align="center"><a href="https://dashboard.heroku.com/new?template=https://github.com/Sumit0045/TheHanCockBot"> <img src="https://img.shields.io/badge/Deploy%20On%20Heroku-black?style=for-the-badge&logo=heroku" width="220" height="38.45"/></a></p>
+
+
+### ᴀɪɴ ᴄᴏᴍᴍɴᴀᴅs
+```
+ᴅᴇʟᴇᴛᴇ - ᴅᴇʟᴇᴛᴇ ғɪʟᴇ.
+ᴅᴇʟᴇᴛᴇᴀʟʟ - ᴅᴇʟᴇᴛᴇ ᴀʟʟ ғɪʟᴇs.
+ᴛᴏᴛᴀʟ - ᴛᴏᴛᴀʟ ғɪʟᴇs.
+ᴄʜᴀɴɴᴇʟ - ᴛᴏᴛᴀʟ ᴄʜᴀɴɴᴇʟs.
+ɪɴᴅᴇx - ɪɴᴅᴇx ᴀʟʟ ғɪʟᴇs.
+ʟᴏɢɢᴇʀ - ɢᴇᴛ ʟᴏɢ ғɪʟᴇ.
+```
---
+++ config.py
@@ -0,0 +1,96 @@
+import re
+from os import environ
+
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+
+# ---------» ʙᴏᴛ sᴇᴛᴛɪɴɢs «--------- 
+BUTTON = True
+USE_CAPTION_FILTER = True
+
+
+# ---------» ᴍᴏɴɢᴏ ᴅʙ ɪɴғᴏʀᴍᴀᴛɪᴏɴ «---------
+DATABASE_URI = "mongodb+srv://MrsFallenBot:MrsFallenBot@cluster0.hsedwn2.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = "Cluster0"
+COLLECTION_NAME = 'Fallen_Files'
+
+
+# ---------» ʙᴏᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ «---------
+SESSION = 'Media_search'
+API_ID = "18719789"
+API_HASH = "a03c27be3e14aac40f62cb4e95207fae"
+BOT_TOKEN = "5895914482:AAGWrQgS_JIpFeuM-A0M_NIL7hYLRxDH8Kk"
+
+
+# ---------» ᴀᴅᴍɪɴs ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴜsᴇʀs «---------
+PREMIUMS = [5709622852, 5416887843, 5807975896]
+ADMINS = [5709622852, 5416887843, 5807975896]
+CHANNELS = [-1001652627420, -1001896500605]
+auth_users = []
+AUTH_USERS = (auth_users + ADMINS) if auth_users else []
+AUTH_CHANNEL = -1001831916389
+AUTH_GROUPS = []
+
+
+# ---------» ʟɪɴᴋ sʜᴏʀᴛɴᴇʀ ᴄᴏɴᴠᴇʀᴛᴇʀ «---------
+URL_SHORTNER_API = environ.get("URL_SHORTNER_API", "https://cyberurl.me/api?api")
+URL_SHORTNER_API_KEY = environ.get("URL_SHORTNER_API_KEY", "7f5cf7665de62a29406d75baa6c13d97286bfec9")
+
+
+
+# ---------» ᴘɪᴄs ʟɪɴᴋs «---------
+default_pics_links = """
+https://graph.org/file/5cc48ce60199bda2ba676.jpg
+
+https://graph.org/file/db5e038720b1578759d7b.jpg
+
+https://graph.org/file/b9468522b4a59624eb169.jpg
+
+https://graph.org/file/164547bf9849d103b0061.jpg
+
+https://graph.org/file/4974ac902f866c40e6197.jpg
+
+https://graph.org/file/b64154792ca4b43e924f1.jpg
+
+"""
+PICS = (environ.get('PICS', default_pics_links)).split()
+
+
+# ---------» sᴛᴀʀᴛ ᴍᴇsᴀᴀɢᴇs «---------
+default_start_msg = """
+ʜᴇʟʟᴏ 
+ɪ ᴀᴍ ᴍᴏᴠɪᴇs ᴘʀᴏᴠɪᴅᴇʀ ʙᴏᴛ
+ɪ ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ᴀʟʟ ᴍᴏᴠɪᴇs  
+ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ 
+ᴍᴀᴋᴇ ᴀᴅᴍɪɴ ᴀɴᴅ ᴇɴᴊᴏʏ ᴜɴʟɪᴍɪᴛᴇᴅ ᴍᴏᴠɪᴇs
+"""
+START_MSG = environ.get('START_MSG', default_start_msg)
+
+
+# --------- ᴄᴜsᴛᴏᴍ ᴄᴀᴘᴛɪᴏɴ ---------
+default_file_caption = """
+📁 {file_name}]
+━━━━━━━━━━━━━━━━━━━━━━━━━━━
+ɪғ ʏᴏᴜ ʟɪᴋᴇ ᴠɪᴅᴇᴏ ᴛʜᴀɴ ᴘʟᴇᴀsᴇ 
+ᴀᴅᴅ sᴏᴍᴇ ᴍᴇᴍʙᴇʀ ᴀɴᴅ sʜᴀʀᴇ ᴛʜᴇ ʟɪɴᴋ
+━━━━━━━━━━━━━━━━━━━━━━━━━━━
+"""
+
+FILE_CAPTION = environ.get('CUSTOM_CAPTION_FILE', default_file_caption)
+
+
+OMDB_API_KEY = environ.get("OMDB_API_KEY", "")
+if FILE_CAPTION.strip() == "":
+    CUSTOM_FILE_CAPTION=None
+else:
+    CUSTOM_FILE_CAPTION=FILE_CAPTION
+if OMDB_API_KEY.strip() == "":
+    API_KEY=None
+else:
+    API_KEY=OMDB_API_KEY
